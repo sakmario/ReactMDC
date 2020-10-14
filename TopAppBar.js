c859dc7b1a5d7b2597d392a071e29cc18ea89e00
Lubomir Sakmary <sakmario@googlemail.com>
2020-10-14 10:38:23 +0200

    Adding last bits of code to TopAppBar

--- TopAppBar.js
+++ TopAppBar.js
@@ -1,6 +1,57 @@
 import React from 'react';
+import classnames from 'classnames';
+import {MDCTopAppBarFoundation, MDCFixedTopAppBarFoundation, MDCShortTopAppBarFoundation} from '@material/top-app-bar';
+import PropTypes from 'prop-types';
 
 export default class TopAppBar extends React.Component {
+  foundation_ = null;
+
+  componentDidMount() {
+    this.initializeFoundation();
+  }
+
+  componentWillUnmount() {
+    this.foundation_.destroy();
+  }
+
+  initializeFoundation = () => {
+    if (this.props.short) {
+      this.foundation_ = new MDCShortTopAppBarFoundation(this.adapter);
+    } else if (this.props.fixed) {
+      this.foundation_ = new MDCFixedTopAppBarFoundation(this.adapter);
+    } else {
+      this.foundation_ = new MDCTopAppBarFoundation(this.adapter);
+    }
+
+    this.foundation_.init();
+  }
+  constructor(props) {
+    super(props);
+    this.topAppBarElement = React.createRef();
+  }
+
+  state = {
+    classList: new Set(),
+    style: {},
+  };
+
+  get classes() {
+    const {classList} = this.state;
+    const {
+      alwaysCollapsed,
+      className,
+      short,
+      fixed,
+      prominent,
+    } = this.props;
+
+    return classnames('mdc-top-app-bar', Array.from(classList), className, {
+      'mdc-top-app-bar--fixed': fixed,
+      'mdc-top-app-bar--short': short,
+      'mdc-top-app-bar--short-collapsed': alwaysCollapsed,
+      'mdc-top-app-bar--prominent': prominent,
+    });
+  }
   render() {
     const {
       title,
@@ -43,4 +94,49 @@ export default class TopAppBar extends React.Component {
     const {style: internalStyle} = this.state;
     return Object.assign({}, internalStyle, style);
   }
+  setStyle = (varName, value) => {
+    const updatedStyle = Object.assign({}, this.state.style);
+    updatedStyle[varName] = value;
+    this.setState({style: updatedStyle});
+  }
+  get adapter() {
+    const {actionItems} = this.props;
+  
+    return {
+      hasClass: (className) => this.classes.split(' ').includes(className),
+      addClass: (className) => this.setState({classList: this.state.classList.add(className)}),
+      removeClass: (className) => {
+        const {classList} = this.state;
+        classList.delete(className);
+        this.setState({classList});
+      },
+      setStyle: this.setStyle,
+      getTopAppBarHeight: () => this.topAppBarElement.current.clientHeight,
+      registerScrollHandler: (handler) => window.addEventListener('scroll', handler),
+      deregisterScrollHandler: (handler) => window.removeEventListener('scroll', handler),
+      registerResizeHandler: (handler) => window.addEventListener('resize', handler),
+      deregisterResizeHandler: (handler) => window.removeEventListener('resize', handler),
+      getViewportScrollY: () => window.pageYOffset,
+      getTotalActionItems: () => actionItems && actionItems.length,
+    };
+  }
+  TopAppBarpropTypes = {
+    alwaysCollapsed: PropTypes.bool,
+    short: PropTypes.bool,
+    fixed: PropTypes.bool,
+    prominent: PropTypes.bool,
+    title: PropTypes.string,
+    actionItems: PropTypes.arrayOf(PropTypes.element),
+    navIcon: PropTypes.element,
+  };
+  
+  TopAppBardefaultProps = {
+    alwaysCollapsed: false,
+    short: false,
+    fixed: false,
+    prominent: false,
+    title: '',
+    actionItems: null,
+    navIcon: null,
+  };
 }
